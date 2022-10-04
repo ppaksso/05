2386ae40d8af386233fb596714381ae8963a7c11
ppaksso <113421378+ppaksso@users.noreply.github.com>
2022-10-04 17:19:29 +0900

    04

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int num = 0 ;
+	int sum=0;
+	int x;
+	int i;
 	
-	printf("input a string:");
-	while ((c = getchar() ) != '\n') //입력문자가 개형문자가 나올때까지 반복 엔터를 누를때까지 while문 반복 
-	{
-		if ('0' <= c && c <= '9')// 입력된 문자가 숫자인가? 
-		  num= num + 1;  //그렇다면 하나를 센다. 
-	}
-   
-   printf("the number of digits is %d \n", num);
-   
+	printf("input a number:");
+	scanf("%d",&x);
+	
+	for (i=0;i<=x;i++)
+	  sum=sum+i ;
+	
+	printf("the result is %d",sum);
+	
 	return 0;
 }
